fd784561deb91adef2fd0e7146df00df022c8f41
erensengulgithub <93150381+erensengulgithub@users.noreply.github.com>
2022-05-10 23:44:59 +0300

    Update GasolineService.java

--- GasolineService.java
+++ GasolineService.java
@@ -1,55 +1,34 @@
 
-public class DieselService extends FuelService {
+public class GasolineService extends FuelService{
 	
-	public DieselService(String cP, double lt , boolean dAF, int dAFC, double dAFP) { //constructor
-		super(cP, lt);
-		dAF = discountedAntiFreeze;
-		dAFC = discountedAntiFreezeCount;
-		dAFP = discountedAntiFreezePrice;
-	}
-
-
-	public DieselService(String cP, double lt, boolean dAF) {
+	public GasolineService(String cP,double lt, boolean hC) { // constructor
 		super(cP,lt);
-		dAF=discountedAntiFreeze;
+		hasCoupon = hC;
 	}
 	
-	public DieselService(String cP, double lt, boolean dAF, int dAFC) {
-		super(cP,lt);
-		dAF=discountedAntiFreeze;
-		dAFC=discountedAntiFreezeCount;
-	}
-
-	public boolean discountedAntiFreeze;
-	public static int discountedAntiFreezeCount;
-	public double discountedAntiFreezePrice;
-	
+	public static boolean hasCoupon;
 	public static void displayServiceInfo() {
-		System.out.println("Diesel Service...");
-		System.out.println("Bought " + litersBought + " liters");
+		System.out.println("Gasoline Service...");
+		System.out.println("Bought "+ litersBought + " liters");
 		System.out.println("Car Plate is " + carPlate);
 		System.out.println("The revenue from this service is " + revenue);
-		System.out.println("Bought " + discountedAntiFreezeCount + " discounted anti-freeze");
-		System.out.println();
+		if(hasCoupon == true)
+			System.out.println("Has 10% discount coupon");  
+			System.out.println();
+			
 	}
-	
 	
 	public static double makeTransaction(double price) {
-		double  dieselT;
+		double gasolineT;
 		double liters = litersBought;
-		int itemcount = discountedAntiFreezeCount;
 		
-		dieselT = ( price*liters ) + (antiFreezePrice(25)*itemcount);
-		
-		return dieselT;
+		if (hasCoupon) {
+			gasolineT = price*liters*(9/10);
+		}
+		else
+			gasolineT = price*liters;
+			
+		return gasolineT;
 	}
 
-
-	private static int antiFreezePrice(int i) {
-		
-		return i;
-	}
-	
 }
-
-
